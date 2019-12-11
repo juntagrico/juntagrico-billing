826582090a1addcc4067fbf73bee84cab06ff40b
David Simmen <david.simmen@bluewin.ch>
2019-12-11 23:06:50 +0100

    fix in template path

--- juntagrico_billing/views.py
+++ juntagrico_billing/views.py
@@ -50,7 +50,7 @@ def subscription_bookings(request):
             'daterange_form': daterange_form,
             'bookings': bookings
         })
-        return render(request, "bk/../../juntagrico-billing/juntagrico_billing/templates/jb/subscription_bookings.html",
+        return render(request, "jb/subscription_bookings.html",
                       renderdict)
 
 
