e3070f4a2e9c6d5b98ab3f7e5eb877bdde8391c9
samuel-iseli <>
2020-03-15 16:27:58 +0100

    corrected display of billable in bills admin list

--- juntagrico_billing/admin/bill.py
+++ juntagrico_billing/admin/bill.py
@@ -1,8 +1,19 @@
 from juntagrico.admins import BaseAdmin
 from juntagrico_billing.admin.payment_inline import PaymentInline
+from juntagrico_billing.entity.bill import Bill
 
 
 class BillAdmin(BaseAdmin):
-    raw_id_fields = ['billable', 'business_year']
-    list_display = ['business_year', 'billable', 'bill_date', 'amount']
+    raw_id_fields = ['business_year']
+    list_display = ['business_year', 'billable_inst', 'bill_date', 'amount']
     inlines = [PaymentInline, ]
+
+    def billable_inst(self, bill):
+        """
+        Get the concrete polymorphic instance.
+        Otherwise the admin list display only shows the abstract base class (Billable).
+        """
+        return bill.billable.get_real_instance()
+
+    billable_inst.short_description = Bill._meta.get_field('billable').verbose_name
+    
