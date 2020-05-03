3138efe844098d790316b9eb4046cb0b72a3b6d5
David Simmen <david.simmen@bluewin.ch>
2020-05-03 08:20:46 +0200

    fix in url name

--- juntagrico_billing/urls.py
+++ juntagrico_billing/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('jb/bills_setyear', views.bills_setyear, name='bills-setyear'),
     path('jb/bills_generate', views.bills_generate, name='bills-generate'),
     path('jb/bills_delete/<int:id>', views.bills_delete, name='bills-delete'),
-    path('jb/bills/user', views.bills, name='user-bills '),
+    path('jb/bills/user', views.bills, name='user-bills'),
 
     # legacy
     path('jb/subscription_bookings', views_legacy.subscription_bookings)
