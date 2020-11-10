340da1d3076f5e22e16cfcbb89ff567720f25c09
samuel-iseli <>
2020-11-10 23:39:55 +0100

    added payment_saved lifecycle function to mark a bill as paid

---
+++ juntagrico_billing/models.py
@@ -0,0 +1,6 @@
+from django.db.models import signals
+from juntagrico_billing.entity.bill import Payment
+from juntagrico_billing.lifecycle.payment import payment_saved
+
+# connect signals to lifecycle functions
+signals.post_save.connect(payment_saved, sender=Payment)
