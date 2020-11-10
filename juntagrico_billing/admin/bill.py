54efabf946ff273cef2972ea800c906129a6243e
samuel-iseli <>
2020-11-10 23:16:03 +0100

    added search in bill admin

--- juntagrico_billing/admin/bill.py
+++ juntagrico_billing/admin/bill.py
@@ -6,5 +6,6 @@ from juntagrico_billing.entity.bill import Bill
 
 class BillAdmin(BaseAdmin):
     readonly_fields = ['business_year']
+    search_fields = ['id', 'member__first_name', 'member__last_name']
     list_display = ['id', 'business_year', 'member', 'bill_date', 'item_kinds', 'amount', 'paid']
     inlines = [BillItemInline, PaymentInline, ]
