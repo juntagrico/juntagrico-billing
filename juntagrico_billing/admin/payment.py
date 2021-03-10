a1613d9a7223b621ea7595ff9d72c6209f773414
David Simmen <david.simmen@bluewin.ch>
2021-03-10 18:53:56 +0100

    pep8

--- juntagrico_billing/admin/payment.py
+++ juntagrico_billing/admin/payment.py
@@ -5,6 +5,7 @@ class PaymentAdmin(BaseAdmin):
     raw_id_fields = ['bill']
     list_display = ['bill', 'paid_date', 'amount', 'type']
     exclude = ['unique_id']
+    
 
 class PaymentTypeAdmin(BaseAdmin):
     pass
