c1b193d0303a5d2e6028ef20bdb8dfe2d9a7edde
samuel-iseli <>
2021-03-21 16:02:07 +0100

    change admin visibility of payment unique_id

--- juntagrico_billing/admin/payment_inline.py
+++ juntagrico_billing/admin/payment_inline.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
 from juntagrico_billing.entity.payment import Payment
 
 
 class PaymentInline(admin.TabularInline):
     model = Payment
     extra = 1
+    exclude = ['unique_id']
