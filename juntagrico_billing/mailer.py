33552b4e0f93bcdc24dc081a08252d7f8c7b95a6
samuel-iseli <>
2022-04-18 17:48:30 +0200

    removed unused send_bill_share function

--- juntagrico_billing/mailer.py
+++ juntagrico_billing/mailer.py
@@ -2,24 +2,8 @@ from django.template.loader import get_template
 from django.utils.translation import gettext as _
 from juntagrico.config import Config
 from juntagrico.mailer import EmailSender, organisation_subject, base_dict
-from juntagrico.management.commands.mailtexts import get_server
 
 from juntagrico_billing.entity.settings import Settings
-
-
-def send_bill_share(bill, share, member):
-    plaintext = get_template('mails/bill_share.txt')
-    d = {
-        'member': member,
-        'bill': bill,
-        'share': share,
-        'serverurl': get_server()
-    }
-    content = plaintext.render(d)
-    EmailSender.get_sender(
-        organisation_subject(_('Bill {0}').format(Config.vocabulary('share'))),
-        content,
-    ).send_to(member.email)
 
 
 def send_bill_notification(bill):
