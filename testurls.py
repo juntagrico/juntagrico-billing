4c6ddc5d1e4bc63fae3005aea349857bc53e5d27
samuel-iseli <>
2022-04-18 17:48:03 +0200

    change django.conf.url to django.url.re_path

--- testurls.py
+++ testurls.py
@@ -1,13 +1,14 @@
 """
 test URL Configuration for juntagrico_billing development
 """
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 import juntagrico
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('juntagrico.urls')),
-    url(r'^', include('juntagrico_billing.urls')),
-    url(r'^$', juntagrico.views.home),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^impersonate/', include('impersonate.urls')),
+    re_path(r'^', include('juntagrico.urls')),
+    re_path(r'^', include('juntagrico_billing.urls')),
+    re_path(r'^$', juntagrico.views.home),
 ]
