cedfa735c4c7d30ac14b9480e6e95919a75be740
pascalfree <pascal.free@ihdg.ch>
2024-02-17 15:05:29 +0100

    refactor all test setups

--- test/test_billbookings.py
+++ test/test_billbookings.py
@@ -5,44 +5,42 @@ from juntagrico_billing.models.bill import BusinessYear, BillItem, BillItemType
 from juntagrico_billing.models.payment import Payment, PaymentType
 from juntagrico_billing.util.billing import create_bill
 from juntagrico_billing.util.bookings import get_bill_bookings, get_payment_bookings
-from test.test_base import SubscriptionTestBase
+from . import BillingTestCase
 
 
-class BillBookingsTest(SubscriptionTestBase):
+class BillBookingsTest(BillingTestCase):
 
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
 
-        self.year = BusinessYear.objects.create(start_date=date(2018, 1, 1),
-                                                end_date=date(2018, 12, 31),
-                                                name="2018")
-
-        self.paymenttype = PaymentType.objects.create(
+        cls.paymenttype = PaymentType.objects.create(
             name="ABS",
             iban="CH4108390031346010006",
-            booking_account="1010")
-
-        self.extrasubs = SubscriptionPart.objects.create(
-            subscription=self.subscription,
-            activation_date=date(2018, 1, 1),
-            type=self.extrasub_type)
-
-        items = list(self.subscription.parts.all())
-
-        self.bill = create_bill(items, self.year, self.year.start_date, 0.0)
-
-        self.payment1 = Payment.objects.create(
-            bill=self.bill,
-            paid_date=date(2018, 2, 1),
-            amount=500.0,
-            type=self.paymenttype
+            booking_account="1010"
         )
 
-        self.payment2 = Payment.objects.create(
-            bill=self.bill,
+        cls.extrasubs = SubscriptionPart.objects.create(
+            subscription=cls.subscription,
+            activation_date=date(2018, 1, 1),
+            type=cls.extrasub_type
+        )
+
+        items = list(cls.subscription.parts.all())
+        cls.bill = create_bill(items, cls.year, cls.year.start_date, 0.0)
+
+        cls.payment1 = Payment.objects.create(
+            bill=cls.bill,
+            paid_date=date(2018, 2, 1),
+            amount=500.0,
+            type=cls.paymenttype
+        )
+
+        cls.payment2 = Payment.objects.create(
+            bill=cls.bill,
             paid_date=date(2018, 7, 2),
             amount=700.0,
-            type=self.paymenttype
+            type=cls.paymenttype
         )
 
     def test_bill_bookings(self):
@@ -83,19 +81,14 @@ class BillBookingsTest(SubscriptionTestBase):
         self.assertEquals('4321', booking.member_account)
 
 
-class BillWithCustomItemBookingsTest(SubscriptionTestBase):
+class BillWithCustomItemBookingsTest(BillingTestCase):
     def test_get_bill_bookings(self):
-        year = BusinessYear.objects.create(start_date=date(2018, 1, 1),
-                                           end_date=date(2018, 12, 31),
-                                           name="2018")
-        year.save()
-
         item_type1 = BillItemType(name='Custom Item 1', booking_account='2211')
         item_type1.save()
         item_type2 = BillItemType(name='Custom Item 2', booking_account='2212')
         item_type2.save()
 
-        bill = create_bill(self.subscription.parts.all(), year, year.start_date, 0.0)
+        bill = create_bill(self.subscription.parts.all(), self.year, self.year.start_date, 0.0)
         item = BillItem(bill=bill, custom_item_type=item_type1, amount=100.0)
         item.save()
         item = BillItem(bill=bill, custom_item_type=item_type2, amount=200.0)
@@ -103,12 +96,12 @@ class BillWithCustomItemBookingsTest(SubscriptionTestBase):
         bill.save()
 
         # get bookigs
-        bookings = get_bill_bookings(year.start_date, year.end_date)
+        bookings = get_bill_bookings(self.year.start_date, self.year.end_date)
 
         self.assertEquals(3, len(bookings))
 
         booking = bookings[1]
-        self.assertEquals(year.start_date, booking.date)
+        self.assertEquals(self.year.start_date, booking.date)
         self.assertEquals("500012", booking.docnumber)
         self.assertEquals("Rg 1: Custom Item 1 Michael Test", booking.text)
         self.assertEquals("1100", booking.debit_account)
@@ -117,7 +110,7 @@ class BillWithCustomItemBookingsTest(SubscriptionTestBase):
         self.assertEquals(100.0, booking.price)
 
         booking = bookings[2]
-        self.assertEquals(year.start_date, booking.date)
+        self.assertEquals(self.year.start_date, booking.date)
         self.assertEquals("500013", booking.docnumber)
         self.assertEquals("Rg 1: Custom Item 2 Michael Test", booking.text)
         self.assertEquals("1100", booking.debit_account)
@@ -131,25 +124,20 @@ class BillWithCustomItemBookingsTest(SubscriptionTestBase):
         should result in bookings with credit and debit
         exchanged and positive amount.
         """
-        year = BusinessYear.objects.create(start_date=date(2018, 1, 1),
-                                           end_date=date(2018, 12, 31),
-                                           name="2018")
-        year.save()
-
         item_type1 = BillItemType(name='Custom Item 1', booking_account='2211')
         item_type1.save()
 
-        bill = create_bill(self.subscription.parts.all(), year, year.start_date, 0.0)
+        bill = create_bill(self.subscription.parts.all(), self.year, self.year.start_date, 0.0)
         item = BillItem(bill=bill, custom_item_type=item_type1, amount=-100.0)
         item.save()
 
         # get bookigs
-        bookings = get_bill_bookings(year.start_date, year.end_date)
+        bookings = get_bill_bookings(self.year.start_date, self.year.end_date)
 
         self.assertEquals(2, len(bookings))
 
         booking = bookings[1]
-        self.assertEquals(year.start_date, booking.date)
+        self.assertEquals(self.year.start_date, booking.date)
         self.assertEquals("500012", booking.docnumber)
         self.assertEquals("Rg 1: Custom Item 1 Michael Test", booking.text)
 
