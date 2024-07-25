4b35899fe4c419bfcbbcae46343e74522ab0bc14
samuel-iseli <3380098+samuel-iseli@users.noreply.github.com>
2024-07-25 22:34:23 +0200

    make businessyear editable in admin

--- juntagrico_billing/admin/bill.py
+++ juntagrico_billing/admin/bill.py
@@ -40,7 +40,6 @@ do_publish_bills.short_description = _("Publish bills")
 
 
 class BillAdmin(BaseAdmin):
-    readonly_fields = ['business_year']
     search_fields = ['id', 'member__first_name', 'member__last_name']
     list_display = [
         'id', 'business_year', 'member', 'bill_date', 'item_kinds',
