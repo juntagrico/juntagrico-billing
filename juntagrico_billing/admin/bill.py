1bb1f4b7a62f27d221685bde57298f6205d0b434
samuel-iseli <3380098+samuel-iseli@users.noreply.github.com>
2025-02-26 23:01:30 +0100

    added filter for open amount

--- juntagrico_billing/admin/bill.py
+++ juntagrico_billing/admin/bill.py
@@ -1,7 +1,7 @@
 from django.utils.translation import gettext as _
 from django.urls import reverse
 from django.utils.html import mark_safe
-from django.contrib.admin import display
+from django.contrib.admin import display, SimpleListFilter
 from juntagrico.admins import BaseAdmin
 
 from juntagrico_billing.admin.billitem_inline import BillItemInline
@@ -47,12 +47,31 @@ def do_update_vat(modeladmin, request, queryset):
 do_update_vat.short_description = _("Update VAT (from settings)")
 
 
+class OpenAmountFilter(SimpleListFilter):
+    title = _('Open amount')
+    parameter_name = 'open_amount_filter'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('open', _('Open amount > 0')),
+            ('overpaid', _('Open amount < 0 (overpaid)')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'open':
+            return [obj for obj in queryset.all() if obj.amount_open > 0]
+        if self.value() == 'overpaid':
+            objects = [obj for obj in queryset.all() if obj.amount_open < 0]
+            print("%d objects" % len(objects))
+            return queryset.filter(id__in=[obj.id for obj in objects])
+
+
 class BillAdmin(BaseAdmin):
     search_fields = ['id', 'member__first_name', 'member__last_name']
     list_display = [
         'id', 'business_year', 'member', 'bill_date', 'item_kinds',
         'amount_f', 'amount_open_f', 'paid', 'published', 'user_bill_link']
-    list_filter = ['paid', 'published', 'notification_sent', 'business_year']
+    list_filter = ['paid', 'published', 'notification_sent', 'business_year', OpenAmountFilter]
     readonly_fields = ['vat_rate', 'vat_amount', 'amount_paid', 'amount_open']
     inlines = [BillItemInline, PaymentInline, ]
     actions = [
