37cf7cf6e862437ed9318c77e2ada30112601bc0
Samuel Iseli <samueliseli@hey.com>
2025-11-24 17:16:51 +0100

    reverted unwanted changes

--- testurls.py
+++ testurls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('impersonate/', include('impersonate.urls')),
     path('', include('juntagrico.urls')),
     path('', include('juntagrico_billing.urls')),
-    path('jcr/', include('juntagrico_contribution.urls')),
 ]
