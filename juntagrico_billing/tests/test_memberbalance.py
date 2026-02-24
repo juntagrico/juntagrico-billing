ba06b7d20de57b2b991d3cbdb173be65cf083fd9
pascalfree <pascal.free@ihdg.ch>
2026-02-24 23:10:53 +0100

    add tests

--- juntagrico_billing/tests/test_memberbalance.py
+++ juntagrico_billing/tests/test_memberbalance.py
@@ -10,24 +10,6 @@ class MemberBalanceTest(BillingTestCase):
     def setUpTestData(cls):
         super().setUpTestData()
 
-        def create_bill(member, date, amount):
-            bill = Bill.objects.create(
-                business_year=cls.year, member=member, published=True,
-                bill_date=date, booking_date=date)
-            BillItem.objects.create(
-                bill=bill, custom_item_type=cls.item_type1,
-                amount=amount
-            )
-            bill.save()
-            return bill
-
-        def create_payment(bill, amount, date):
-            payment = Payment.objects.create(
-                bill=bill, amount=amount, paid_date=date, type=cls.payment_type
-            )
-            payment.save()
-            return payment
-
         # create a payment type
         cls.payment_type = PaymentType.objects.create(name='Test Payment Type')
 
@@ -37,19 +19,19 @@ class MemberBalanceTest(BillingTestCase):
         cls.item_type1 = BillItemType.objects.create(name='Test Item Type', booking_account='2211')
 
         # create some bills
-        cls.bill1 = create_bill(cls.member1, date(2024, 10, 1), 1200)
-        cls.bill2 = create_bill(cls.member1, date(2024, 11, 1), 1300)
-        cls.bill3 = create_bill(cls.member1, date(2025, 2, 1), 1400)
-        cls.bill4 = create_bill(cls.member2, date(2024, 10, 1), 1500)
-        cls.bill5 = create_bill(cls.member2, date(2024, 11, 1), 1600)
+        cls.bill1 = cls.create_bill(cls.member1, cls.item_type1, date(2024, 10, 1), 1200)
+        cls.bill2 = cls.create_bill(cls.member1, cls.item_type1, date(2024, 11, 1), 1300)
+        cls.bill3 = cls.create_bill(cls.member1, cls.item_type1, date(2025, 2, 1), 1400)
+        cls.bill4 = cls.create_bill(cls.member2, cls.item_type1, date(2024, 10, 1), 1500)
+        cls.bill5 = cls.create_bill(cls.member2, cls.item_type1, date(2024, 11, 1), 1600)
 
         # create some payments
-        create_payment(cls.bill1, 400, date(2024, 10, 15))
-        create_payment(cls.bill1, 800, date(2024, 11, 15))
-        create_payment(cls.bill2, 1500, date(2024, 11, 15))     # overpayment 200
-        create_payment(cls.bill3, 1400, date(2025, 2, 15))
+        cls.create_payment(cls.payment_type, cls.bill1, 400, date(2024, 10, 15))
+        cls.create_payment(cls.payment_type, cls.bill1, 800, date(2024, 11, 15))
+        cls.create_payment(cls.payment_type, cls.bill2, 1500, date(2024, 11, 15))  # overpayment 200
+        cls.create_payment(cls.payment_type, cls.bill3, 1400, date(2025, 2, 15))
         # no payment for bill4, -1500
-        create_payment(cls.bill5, 1500, date(2024, 10, 15))
+        cls.create_payment(cls.payment_type, cls.bill5, 1500, date(2024, 10, 15))
 
     def test_member_balance(self):
         balances = get_memberbalances(date(2024, 12, 31))
